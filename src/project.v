c8740cd21bc0d1589e6990c120680c850605b748
Ryan Wang <ryan.jiahua.wang@gmail.com>
2025-10-19 22:54:11 -0400

    Got rid of assign statements for output that were forcing outputsto be gibberish.

--- src/project.v
+++ src/project.v
@@ -17,8 +17,8 @@ module tt_um_uwasic_onboarding_ryan_wang (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
+  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  // assign uio_out = 0; 
   assign uio_oe  = 8'hFF;
 
   // Wires to refer to the values of the registers
